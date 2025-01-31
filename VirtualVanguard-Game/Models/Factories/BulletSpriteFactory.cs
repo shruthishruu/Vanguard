6520808acbae42737cb15e566b72b030c237256a
dbiehler <dbiehler@eecs.wsu.edu>
2025-01-30 18:21:04 -0800

    Re-initialized Game

---
+++ VirtualVanguard-Game/Models/Factories/BulletSpriteFactory.cs
@@ -0,0 +1,10 @@
+namespace Models.Factories
+{
+    public class BulletSpriteFactory : SpriteFactory
+    {
+        public override void CreateSprite(string type, int x, int y, int width, int height, string imagePath)
+        {
+            // Implementation for creating a bullet sprite
+        }
+    }
+}
