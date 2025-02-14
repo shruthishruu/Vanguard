ba854563d3421baff0d661331aaee6048179b4ef
dbiehler <dbiehler@eecs.wsu.edu>
2025-02-13 17:20:01 -0800

    started event manager

--- VirtualVanguard-Game/Models/Controls/EventManager.cs
+++ VirtualVanguard-Game/Models/Controls/EventManager.cs
@@ -4,7 +4,10 @@ namespace VirtualVanguard_Game.Models
 {
     public class EventManager : Control
     {
-        public EventManager() { }
+        CharacterEntityFactory characterFactory
+        public EventManager() { 
+            characterFactory = new CharacterEntityFactory();
+        }
 
         public override void Update()
         {
@@ -13,8 +16,16 @@ namespace VirtualVanguard_Game.Models
 
         public void Update(Microsoft.Xna.Framework.GameTime gameTime)
         {
+            
             // Printing the total game time in seconds
             Console.WriteLine($"Total game time: {gameTime.TotalGameTime.TotalSeconds} seconds");
+
+            // this is where you add events
+            // example of spawning enemy
+            if (gameTime.TotalGameTime.TotalSeconds == 5):
+                Console.WriteLine("Spawning enemy");
+                characterFactory.CreateEntity("Enemy", 0, 0, 50, 50, "enemy.png");
+                
         }
 
     }
