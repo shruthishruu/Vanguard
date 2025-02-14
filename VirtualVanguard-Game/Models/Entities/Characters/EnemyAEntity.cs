51a579d17ac6d2b5387b15b66e233347bc5ed72a
Jordan Liebe <jordan.liebe@outlook.com>
2025-02-13 19:59:51 -0800

    Added slow mode for players, two types of enemies, and two types of bosses.

---
+++ VirtualVanguard-Game/Models/Entities/Characters/EnemyAEntity.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace VirtualVanguard_Game.Models
+{
+    public class EnemyAEntity : EnemyEntity
+    {
+        public EnemyAEntity(Vector2 position, int width, int height, Texture2D image) : base(position, width, height, image)
+        {
+        }
+    }
+}
